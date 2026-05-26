3470902b3c8f12e2ab5147a830a832be99b0a877
remostudios2025-debug <remostudios2025@gmail.com>
2026-05-26 23:31:57 +0300

    Delete app directory

--- app/app/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ background: "black", color: "white", minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "28px" }}>
-      REMON ROMANY WEBSITE IS LIVE 🚀
-    </div>
-  );
-}
--- app/layout.js
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, background: "black" }}>{children}</body>
-    </html>
-  );
-}
