3320c7d3048c35f145529a77359baf1e0b00322d
remostudios2025-debug <remostudios2025@gmail.com>
2026-05-26 23:18:12 +0300

    Create page.js

---
+++ app/app/app/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div style={{ background: "black", color: "white", minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "28px" }}>
+      REMON ROMANY WEBSITE IS LIVE 🚀
+    </div>
+  );
+}
